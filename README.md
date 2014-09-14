6c14091214f5a820beec3e60d63d5f8168b890c0
Thomas Nguyen <thomasnguyen@fas.harvard.edu>
2014-09-14 14:06:59 -0400

    Added project files

---
+++ README.md
@@ -0,0 +1,14 @@
+# Project 1
+
+Task
+  Create a single page website using basic HTML and CSS with the following information:
+  Your first and last name
+  A photo of yourself
+  Placeholder links for Project 2, 3 and 4. Include a placeholder for both the live URL of the project as well as the Github URL.
+  Answer the following questions to help the teaching team get to know you:
+
+In 2+ sentences describe who you are and what you do.
+  Why are you taking this course?
+  What previous experience do you have with programming?
+  Are you working on a Mac, PC, or some other platform?
+  Anything else we should know?
---
+++ index.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Thomas Nguyen</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+
+
+<div class="container">
+
+<div class="row">
+	<div class="jumbotron">
+		<h1>Thomas Nguyen</h1>
+        <p>Web Applications Portfolio</p>
+    </div>
+</div>
+
+<p class="text-center"><img src="images/thomas.jpg" alt="thomas" class="img-responsive img-rounded"></p>
+
+<div class="row">
+    <div class="col-md-4">
+    	<div class="well">
+            <p class="lead"> Project 2: Title</p>
+            <p>View: <a href="#">link</a></p>
+            <p>Github: <a href="#">link</a></p>
+        </div>
+	</div>
+
+    <div class="col-md-4">
+    	<div class="well">
+            <p class="lead"> Project 3: Title</p>
+            <p>View: <a href="#">link</a></p>
+            <p>Github: <a href="#">link</a></p>
+        </div>
+	</div>
+
+    <div class="col-md-4">
+    	<div class="well">
+            <p class="lead"> Project 4: Title</p>
+            <p>View: <a href="#">link</a></p>
+            <p>Github: <a href="#">link</a></p>
+        </div>
+	</div>
+</div>
+
+
+<h3 class="page-header">About</h3>
+
+<h4>In 2+ sentences describe who you are and what you do.</h4>
+<p>My name is Thomas Nguyen. I currently live in Charlotte, NC and work as a Project Manager for a large financial insitution. When I have spare time, I work on my house and hang out with friends.</p>
+
+<br>
+
+<h4>Why are you taking this course?</h4>
+<p>I am taking this course to enhance my knowledge in web development.</p>
+
+<br>
+
+<h4>What previous experience do you have with programming?</h4>
+<p>My previous experience with programming are associated with the course work completed at Harvard Extension School. Languages include HTML, CSS, PHP, javaScript and Java</p>
+
+<br>
+
+<h4>Are you working on a Mac, PC, or some other platform?</h4>
+<p>I am working on a Mac</p>
+
+<br>
+
+<h4>Anything else we should know?</h4>
+<p>I'm excited in learning dynamic web development and look forward in having a great semester at HES.</p>
+
+<br>
+
+</div><!--container-->
+
+
+</body>
+</html>
